24e34c8773f3ae5b507c7ee1d74c2c8f2123203b
Vivek Sahu <viveksahu1762@gmail.com>
2024-01-14 12:40:16 +0530

    Created Next js template app

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import HomePage from "@/pages/HomePage";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default page;
