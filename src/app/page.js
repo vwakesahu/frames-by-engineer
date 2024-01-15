0b802250d426865ec9e12ab3a605a774bfc2a106
vwakesahu <viveksahu1762@gmail.com>
2024-01-15 10:28:26 +0530

    Added the main page structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import { Nav } from "@/modules/Nav";
 import HomePage from "@/pages/HomePage";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="p-4 pt-28">
       <HomePage />
     </div>
   );
