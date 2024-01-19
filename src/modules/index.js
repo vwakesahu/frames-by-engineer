0e48b2b255f3928d4062f32279281e20d8df7138
Vivek Sahu <viveksahu1762@gmail.com>
2024-01-19 16:12:44 +0530

    Optimized: imports

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,4 @@
 export { default as HeroSection } from "./HeroSection";
 export { Nav } from "./Nav";
 export { default as Footer } from "./Footer";
+export { default as ContactUs } from "./ContactUs";
