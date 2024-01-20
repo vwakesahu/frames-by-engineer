6c530b1882d06a0c403ebbf81cb1c7ac7d60a4c4
Vivek Sahu <viveksahu1762@gmail.com>
2024-01-20 21:12:52 +0530

    Fetching: frames Whenever app gets mounted

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
+'use client'
+import { useStateValue } from "@/context/StateProvider";
+import { actionType } from "@/context/reducer";
 import { Nav } from "@/modules/Nav";
 import HomePage from "@/pages/HomePage";
-import React from "react";
+import { getAllFrames } from "@/utils/firebaseFunctions";
+import React, { useEffect } from "react";
 
 const page = () => {
+
+  const [{ frames }, dispatch] = useStateValue();
+
+  const fetchData = async () => {
+    await getAllFrames().then((data) => {
+      // console.log(data);
+      dispatch({
+        type: actionType.SET_FRAMES,
+        frames: data,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
       <HomePage />
