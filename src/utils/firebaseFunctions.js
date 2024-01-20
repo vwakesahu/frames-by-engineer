0ec2188e58436804968a0c25bbdde24f62639f3f
Vivek Sahu <viveksahu1762@gmail.com>
2024-01-20 21:17:37 +0530

    Functions: Adding and Getting frames from firebase

--- src/utils/firebaseFunctions.js
+++ src/utils/firebaseFunctions.js
@@ -1,26 +1,26 @@
 import {
-    collection,
-    doc,
-    getDocs,
-    orderBy,
-    query,
-    setDoc,
-  } from "firebase/firestore";
-  import { firestore } from "../firebase.config";
-  
-  // Saving new Item
-  export const saveItem = async (data) => {
-    await setDoc(doc(firestore, "foodItems", `${Date.now()}`), data, {
-      merge: true,
-    });
-  };
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+} from "firebase/firestore";
+import { firestore } from "../../firebase.config";
 
-  // get all food items
-export const getAllFoodItems = async () => {
+//saves new item to firebase
+export const saveItem = async (data) => {
+  await setDoc(doc(firestore, "frames", `${Date.now()}`), data, {
+    merge: true,
+  });
+};
+
+
+//gets frames from firebase
+export const getAllFrames = async () => {
   const items = await getDocs(
-    query(collection(firestore, "foodItems"), orderBy("id", "desc"))
+    query(collection(firestore, "frames"), orderBy("id", "desc"))
   );
 
   return items.docs.map((doc) => doc.data());
 };
-  
