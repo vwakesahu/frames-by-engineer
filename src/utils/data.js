6613a59b8bed6fd528576969b4d8c0521fc55b0c
Vivek Sahu <viveksahu1762@gmail.com>
2024-01-20 21:13:17 +0530

    Dummy data for uploading

---
+++ src/utils/data.js
@@ -0,0 +1,89 @@
+export const categories = [
+    {
+      id: 1,
+      name: "Marvel",
+      urlParamName: "marvel",
+    },
+    {
+      id: 2,
+      name: "Anime",
+      urlParamName: "anime",
+    },
+    {
+      id: 3,
+      name: "Motivational",
+      urlParamName: "motivational",
+    },
+    {
+      id: 4,
+      name: "Models",
+      urlParamName: "model",
+    },
+    {
+      id: 5,
+      name: "Bollywood",
+      urlParamName: "bollywood",
+    },
+    {
+      id: 6,
+      name: "Icecreams",
+      urlParamName: "icecreams",
+    },
+  
+    {
+      id: 7,
+      name: "Show Piece",
+      urlParamName: "show-piece",
+    },
+  ];
+  
+  export const frameColors = [
+    {
+      id: 1,
+      color: "Black",
+      urlParamName: "black",
+    },
+    {
+      id: 2,
+      color: "Pink",
+      urlParamName: "pink",
+    },
+    {
+      id: 3,
+      color: "Red",
+      urlParamName: "red",
+    },
+    {
+      id: 4,
+      color: "Blue",
+      urlParamName: "blue",
+    },
+  ];
+  
+  export const frameDimension = [
+    {
+      id: 1,
+      dimension: "Vertical",
+      urlParamName: "Vertical",
+    },
+    {
+      id: 2,
+      dimension: "Horizontal",
+      urlParamName: "Vertical",
+    },
+  ];
+  
+  export const frameMaterials = [
+    {
+      id: 1,
+      name: "Wood",
+    },
+    {
+      id: 2,
+      name: "Metal",
+    },
+    {
+      id: 3,
+      name: "Plastic",
+    },
+  ];
