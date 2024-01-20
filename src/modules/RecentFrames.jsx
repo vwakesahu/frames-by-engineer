b016ee5328be429a4afbb355796f238c9c1cc727
Vivek Sahu <viveksahu1762@gmail.com>
2024-01-20 21:13:52 +0530

    Added: Recent Frames

---
+++ src/modules/RecentFrames.jsx
@@ -0,0 +1,29 @@
+"use client";
+import { useStateValue } from "@/context/StateProvider";
+import React, { useEffect } from "react";
+
+const RecentFrames = () => {
+  const [{ frames }, dispatch] = useStateValue();
+  useEffect(() => {
+    console.log(frames);
+  }, [frames]);
+
+  return (
+    <div>
+      {/* {frames &&
+        frames.map((frame) => (
+          <div>
+            <img src={frame.imageURL} className="w-10" />
+            <p>{frame.category}</p>
+            <p>{frame.color}</p>
+            <p>{frame.dimensions}</p>
+            <p>{frame.glass}</p>
+            <p>{frame.height}</p>
+            <p>{frame.width}</p>
+          </div>
+        ))} */}
+    </div>
+  );
+};
+
+export default RecentFrames;
--- src/modules/index.js
+++ src/modules/index.js
@@ -2,3 +2,4 @@ export { default as HeroSection } from "./HeroSection";
 export { Nav } from "./Nav";
 export { default as Footer } from "./Footer";
 export { default as ContactUs } from "./ContactUs";
+export { default as RecentFrames } from "./RecentFrames";
