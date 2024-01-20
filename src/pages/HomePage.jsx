1d316c027d6e5eb78690e127e709d38bfe5ab964
Vivek Sahu <viveksahu1762@gmail.com>
2024-01-20 21:17:01 +0530

    Added: Recent Frames Section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
-import { HeroSection } from "@/modules";
+import { RecentFrames, HeroSection } from "@/modules";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div className="h-screen">
       <HeroSection />
+      <RecentFrames />
     </div>
   );
 };
